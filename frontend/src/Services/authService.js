97ecbb07df3f59378ef0ba4d43efd6ae7ef8b29f
SaiSree <saisreelikhitha9@gmail.com>
2025-09-06 19:38:13 +0530

    Update authService.js

--- frontend/src/Services/authService.js
+++ frontend/src/Services/authService.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 const API_URL = process.env.REACT_APP_API_URL;
 
-export const signup = (data) => axios.post(`${API_URL}api/signup`, data);
-export const login = (data) => axios.post(`${API_URL}api/login`, data);
+export const signup = (data) => axios.post(`${API_URL}signup`, data);
+export const login = (data) => axios.post(`${API_URL}login`, data);
