90c9629ec8335ad41ea6a0a3072bfc1eb1b3c26f
SaiSree <saisreelikhitha9@gmail.com>
2025-09-06 15:45:26 +0530

    Update index.js

--- backend/models/index.js
+++ backend/models/index.js
@@ -2,6 +2,6 @@ const sequelize = require('../config/db');
 const User = require('./User');
 const Expense = require('./Expense');
 
-sequelize.sync();
+
 
 module.exports = { sequelize, User, Expense };
