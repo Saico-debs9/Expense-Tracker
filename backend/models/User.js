c0187b52989cce4d932b57c7929e752733b289ff
raghu <raghu@infilect.com>
2025-09-10 21:19:24 +0530

    update user.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -2,6 +2,11 @@ const { DataTypes } = require('sequelize');
 const sequelize = require('../config/db');
 
 const User = sequelize.define('User', {
+   id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
   email: {
     type: DataTypes.STRING,
     unique: true,
@@ -10,6 +15,15 @@ const User = sequelize.define('User', {
   password: {
     type: DataTypes.STRING,
     allowNull: false
+  },
+  googleId: {
+    type: DataTypes.STRING,
+    unique: true,
+    allowNull: true
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: true
   }
 });
 
