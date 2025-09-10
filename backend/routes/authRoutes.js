054789de96f308de6c361ae653aba128022240aa
SaiSree <saisreelikhitha9@gmail.com>
2025-09-10 23:49:51 +0530

    Update authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { signup, login, test } = require('../controllers/authController');
+const { signup, login, test, googlelogin } = require('../controllers/authController');
 
 router.post('/signup', signup);
 router.post('/login', login);
 router.get('/', test);
+router.post("/google-login", googlelogin);
 
 module.exports = router;
