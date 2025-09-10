6fdab333ee35a15463085d8912b7c92d42b3125d
raghu <raghu@infilect.com>
2025-09-10 23:52:32 +0530

    added route for google login

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { signup, login, test, googlelogin } = require('../controllers/authController');
+const { signup, login, test, googleLogin } = require('../controllers/authController');
 
 router.post('/signup', signup);
 router.post('/login', login);
 router.get('/', test);
-router.post("/google-login", googlelogin);
+router.post("/google-login", googleLogin);
 
 module.exports = router;
